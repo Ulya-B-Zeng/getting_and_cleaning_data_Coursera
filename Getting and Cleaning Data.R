b50966ef9500efbc4c9ff739da29f7a433a966f0
Ulya-B-Zeng <zengbentuoxmu@outlook.com>
2022-06-17 15:00:26 +0800

    R script and tidy dataset

--- Getting and Cleaning Data.R
+++ Getting and Cleaning Data.R
@@ -1,4 +1,5 @@
 setwd(".\\")
+
 ##1. read the txt files.
 data_train <- read.table(".\\UCI HAR Dataset\\train\\x_train.txt")
 data_test <-read.table(".\\UCI HAR Dataset\\test\\X_test.txt")
@@ -27,8 +28,45 @@ if(!identical(substr(names(mean),start = 1,stop = 9),
 label_meanstd <- names(mean)
 time <- grep("^t",label_meanstd)
 FFT <-grep("^f",label_meanstd)
+data_time <- data.frame(matrix(ncol=0,nrow=nrow(data)))
+data_FFT <- data.frame(matrix(ncol=0,nrow=nrow(data)))
 for(i in time){
-  
+  data_time[[names(mean)[i]]] <- mean[,i]
+  data_time[[names(std[i])]] <- std[,i]
 }
+for(i in FFT){
+  data_FFT[[names(mean)[i]]] <- mean[,i]
+  data_FFT[[names(std[i])]] <- std[,i]
+}
+
+##5. rename
+names_time <- names(data_time)
+names_FFT <- names(data_FFT)
+names_time <- sub("^t","time.",names_time)
+names_FFT <- sub("^f","FFT.",names_FFT)
+originalsign <- c("Body","Gravity","Acc","Gyro","Jerk","Mag","-mean[()]","-std[()]","[(]","[)]")
+replacemsign <- c("Body.","Gravity.","Accelerometer.","Angular-velocity.","Jerk-signals.","Magnitude.",
+                  "mean.","std-dev.","","")
+for(i in 1:length(originalsign)){
+  if(length(grep(originalsign[i],names_time))!=0){
+    names_time <- sub(originalsign[i],replacemsign[i],names_time)
+  }
+  if(length(grep(originalsign[i],names_FFT))!=0){
+    names_FFT <- sub(originalsign[i],replacemsign[i],names_FFT)
+  }
+}
+
+names(data_time) <- names_time
+names(data_FFT) <- names_FFT
+
+##6.Output
+data2 <- cbind.data.frame(data_time,data_FFT)
+write.csv(x = data2,file = "tidy_data.csv")
+## XLSX with two sheets
+install.packages("openxlsx")
+library(openxlsx)
+xlsxsheet <- list("Time"=data_time,"Fast Fourier Transform"=data_FFT)
+write.xlsx(xlsxsheet,file="tidy_data.xlsx")
+
 
 
